3845951a34d1c560a1eae24c0535cc85c95a40b9
hadoizz <82988056+hadoizz@users.noreply.github.com>
2023-09-09 11:27:05 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://presets-13dfe01384d2.herokuapp.com/';
+$config['base_url'] = 'https://presets-13dfe01384d2.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
